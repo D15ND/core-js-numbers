bb740b5d373110c74941d9a8db8831b14e06ee19
ILYA <106765406+D15ND@users.noreply.github.com>
2025-09-15 02:48:55 +0300

    feat: add some solutions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -199,8 +199,16 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 0) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -218,8 +226,9 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const newNum = Number(value);
+  return Number.isNaN(newNum) ? def : newNum;
 }
 
 /**
